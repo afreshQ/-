60a6b51715384b64e2cdabeaaa864aaa84a28bb4
lastWhi <1040044589@qq.com>
2019-10-27 22:20:59 +0800

    实现登陆后显示个人信息

--- reeble_news/src/pages/Profile.vue
+++ reeble_news/src/pages/Profile.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="container">
-    <div class="row1">
-      <img src="http://img4.imgtn.bdimg.com/it/u=2841002847,3345892877&fm=26&gp=0.jpg" alt />
-      <div class="profile">
-        <span class="iconfont iconxingbienan"></span>
-        <span class="name">火星网友</span>
-        <p class="time hui">2019-10-25</p>
+    <router-link to="/editprofile">
+      <div class="row1">
+        <img v-if="headImg" :src="'http://111.230.181.206:3000' +headImg" />
+        <img v-else src="http://img4.imgtn.bdimg.com/it/u=2841002847,3345892877&fm=26&gp=0.jpg" alt />
+        <div class="profile">
+          <span v-if="gender==1" class="iconfont iconxingbienan"></span>
+          <span v-else class="iconfont iconxingbienv"></span>
+          <span class="name">{{nickname}}</span>
+          <p class="time hui">2019-10-25</p>
+        </div>
+        <span class="hui iconfont iconjiantou1"></span>
       </div>
-      <span class="hui iconfont iconjiantou1"></span>
-    </div>
+    </router-link>
     <div class="row2">
         <cellBar
          label="我的关注"
@@ -29,6 +33,11 @@
          label="设置"
          @sendlabel='toLabel'
          desc=''/>
+         <!-- 添加退出登陆功能 -->
+         <cellBar
+         label="退出登陆"
+         @sendlabel='Logout'
+         desc=''/>
     </div>
   </div>
 </template>
@@ -41,14 +50,54 @@ export default {
   },
   data(){
       return {
-
+        nickname:'',
+        gender:'',
+        headImg:''
       }
   },
   methods:{
       toLabel(label){
         console.log(label);
         
+      },
+      Logout(){
+        //弹出提示框
+        this.$dialog.confirm({
+          message: '确定要退出吗?'
+        })
+        //点击确认执行
+        .then(()=>{
+           //删除本地存储上的token
+          localStorage.removeItem("token");
+          // localStorage.removeItem("userId");
+          this.$router.replace({
+            name:'loginPage'
+          })
+        })
+        // //点击关闭执行
+        .catch(()=>{
+          return;
+        })
+
       }
+  },
+
+  //根据本地存储上的userid去后台拿数据渲染到页面上
+  created(){
+    this.$axios({
+      url:`/user/${localStorage.getItem('userId')}`,
+      method:'get',
+      //还要用token去和后台匹配
+      headers:{
+        Authorization:localStorage.getItem('token')
+      }
+    }).then(response=>{
+      // console.log(response.data);
+      this.nickname=response.data.data.nickname;
+      this.gender=response.data.data.gender;
+      this.headImg=response.data.data.head_img;
+      
+    })
   }
 };
 </script>
