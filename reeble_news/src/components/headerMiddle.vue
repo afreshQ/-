9120615f484c82f4465629b49f590ad788efe951
lastWhi <1040044589@qq.com>
2019-10-28 18:52:58 +0800

    头部组件

---
+++ reeble_news/src/components/headerMiddle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="title">
+      <span @click="$router.back()" class="iconfont iconjiantou2"></span>
+      <h4>{{title}}</h4>
+      <span class="iconfont iconjiantou2 vbhide"></span>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['title']
+}
+</script>
+
+<style>
+    .title{
+      padding:0 4.167vw;
+      line-height: 13.889vw;
+      display: flex;
+      justify-content:space-between;
+      
+    }
+    .vbhide{
+      visibility: hidden;
+    }
+</style>
