3f1f835da629b7deed933d8d34debe7a7638a93a
lastWhi <1040044589@qq.com>
2019-11-01 20:18:08 +0800

    如果输入框有内容就不会失焦消失

---
+++ reeble_news/src/components/commentFloor.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <floor v-if="floorItem.parent" :floorItem="floorItem.parent" />
+        <div class="commentFloor">
+            <div class="header">
+                <span class="name">火星网友</span>
+                <span class="time sz-12">5秒前</span>
+                <span class="btnReply sz-12">回复</span>
+            </div>
+        <div class="content">123</div>
+  </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"floor",
+    props:['floorItem']
+}
+</script>
+
+<style lang="less" scoped>
+.commentFloor{
+    padding: 4.167vw;
+    border: 1px solid #d7d7d7;
+    font-size: 3.889vw;
+    .header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .time{
+            flex:1;
+            padding-left: 4.167vw;
+        }
+    }
+    .content{
+        margin-top: 4.167vw;
+    }
+}
+.sz-12{
+    font-size: 3.333vw;
+    color: #707070;
+}
+</style>
